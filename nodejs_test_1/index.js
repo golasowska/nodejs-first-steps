ae2920390521cc5ef325343c62a1b621aa6fbc9e
Anna Golasowska <annagolasowska@gmail.com>
2018-01-17 18:52:24 +0100

    added exercises files: slides, votes, piratepad

---
+++ nodejs_test_1/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+// stworzenie aplikacji
+const app = express();
+
+const http = require('http').Server(app);
+
+
+const io = require('socket.io') (http);
+
+http.listen(3000, () => {
+  console.log('Express działa!');
+});
+
+app.use( express.static('public'));
+
+let answers = {
+  yes: 0,
+  no: 0
+};
+
+//poinformuj, ze uzytkownik(socket) sie podlaczyl, obsluga eventu z frontu
+io.on('connection', socket=>{
+
+  socket.emit('votes', answers);
+
+  socket.on('myVote', data=>{
+
+    console.log({data});
+
+    if ( ['yes', 'no'].indexOf(data) === -1) return;
+
+    answers[data]++;
+
+    io.emit('votes', answers)
+  });
+});
